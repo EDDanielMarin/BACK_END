e826d7e2f7a5723c621c5b6a29580871ef31bd36
EDDanielMarin <yop_dm@hotmail.com>
2018-08-15 09:24:02 -0500

    errore

--- src/routes/equipos.js
+++ src/routes/equipos.js
@@ -61,8 +61,9 @@ router.delete('/:usuario', middleware.ensureAuthenticated, async (req, res) => {
         if (!usuarios)
             return res.status(404).send({ mesagge: 'no hay equipos del usuario' })
         usuarios.forEach(element => {
-            console.log(element)
+            console.log(element._id)
             Equipo.findByIdAndRemove(element._id);
+            console.log(element)
         });
         res.json({
             status: 'Equipo eliminado'
