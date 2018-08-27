80ee0ac7fa89b356a613ed6aa0acd1d7ddabafbb
Santiago Romero <santiago.romero@celec.gob.ec>
2018-08-26 20:41:08 -0500

    cambio hora

--- src/routes/lecturas.js
+++ src/routes/lecturas.js
@@ -81,7 +81,7 @@ router.get('/:equipo/:adc/:ppm/:estado/:voltaje/:mgm3', async (req, res) => {//S
         await EnvioNotificaciones(req.params.equipo, equipoUsuario.usuario, "mgm3", req.params.mgm3);
 
         lectura.codigo = num + 1//En el campo código de la nueva lectura se asigna el valor de num + 1
-        lectura.hora = new Date();//En el campo hora de la nueva lectura se asigna la fecha actual
+        lectura.hora = new Date.now();//En el campo hora de la nueva lectura se asigna la fecha actual
 
         //Se guarda la nueva lectura únicamente cuando se detecta CO
         if (lectura.estado >= 1)
