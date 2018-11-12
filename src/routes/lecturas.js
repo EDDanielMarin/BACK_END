963fbc339ba28ddc5e6bc7285d4ba77b14884001
Santiago Romero <santiago.romero@celec.gob.ec>
2018-11-12 08:15:15 -0500

    Cambio cuenta de correo

--- src/routes/lecturas.js
+++ src/routes/lecturas.js
@@ -188,13 +188,13 @@ async function EnviarNotificacionPorTipo(equipo, usuario, nombre, valor, tipo) {
     var transporter = nodemailer.createTransport({//Se crea el servicio de transporte necesario para poder enviar correos electrónicos
         service: 'gmail',//Se especifica el uso de gmail
         auth: {//Se define el objeto de autenticación especificando la cuenta y contraseña de gmail
-            user: 'raspberry.tfm2018@gmail.com',
+            user: 'raspberrytxtfm2018@gmail.com',
             pass: 'proyectoNode'
         }
     });
 
     var mailOptions = {//Se especifican los detalles del correo a enviar como emisor, receptor, asunto y texto
-        from: 'raspberry.tfm2018@gmail.com',
+        from: 'raspberrytxtfm2018@gmail.com',
         to: cliente.correo,
         subject: notificacion.asunto,
         text: notificacion.descripcion
